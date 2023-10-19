f34c9119a4ee1451b5aaf65ff65fedc7566a48a0
Girish Patel <patelg123@gmail.com>
2023-10-19 15:24:15 +0100

    First commit of script

---
+++ chatgpt.py
@@ -0,0 +1,41 @@
+import openai
+import os
+
+# Set up your API key
+openai.api_key = os.environ["OPENAI_API_KEY"]
+
+# Define a function that takes user input and sends it to ChatGPT
+
+last_response = "";
+
+def chat_gpt(user_input, last_answer):
+    
+    messages = [
+                  { "role": "system", "content": "You are a helpful assistant." },
+                  { "role": "assistant", "content": last_answer},
+                  { "role": "user", "content": user_input}
+                ]
+    
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+        temperature=0,
+        max_tokens=256
+    )
+    
+    return response.choices[0].message.content   
+    
+#https://levelup.gitconnected.com/a-beginners-guide-to-using-the-chatgpt-api-in-python-29c57775113b
+
+# Loop to ask for user input and receive ChatGPT's response
+while True:
+    user_input = input("You: ")
+    if user_input.lower() == "exit":
+        print("ChatGPT: Goodbye!")
+        break
+    
+    response = chat_gpt(user_input, last_response)
+    print("ChatGPT:", response)
+    
+    last_response = response;
+    
